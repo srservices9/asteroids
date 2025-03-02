af2ed6e65e6125044e7247267af39d94da5f52ac
srservices9 <srservicesc@proton.me>
2025-03-01 21:40:06 -0500

    left right rotation in game
    Added left and right rotation logic to update method and initialized
    update method in main.

--- player.py
+++ player.py
@@ -1,5 +1,4 @@
 import pygame
-from main import *
 from circleshape import *
 from constants import * 
 
@@ -20,13 +19,13 @@ class Player(CircleShape):
     def draw(self, screen):
         pygame.draw.polygon(screen, (255, 255, 255), self.triangle(), 2)
 
-    def rotate(self, PLAYER_TURN_SPEED, dt)
+    def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
-            # ?
+            self.rotate(dt * -1)
         if keys[pygame.K_d]:
-            # ?
+            self.rotate(dt)
