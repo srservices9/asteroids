b0ce9f53adff9ea4164672fffbca52c279c84798
srservices9 <srservicesc@proton.me>
2025-03-01 21:22:05 -0500

    Player Draw and Rotate
    Added Player and draw method and initiated player in main to draw. Also
    created rotation and update methods.

--- main.py
+++ main.py
@@ -3,18 +3,21 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     Clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     print("Starting Asteroids!")
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
         Clock.tick(60)
         dt = Clock.tick(60) / 1000
