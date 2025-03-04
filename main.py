134ea808e7f8a8baa703baca49a7aef00793d280
srservices9 <srservicesc@proton.me>
2025-03-03 20:47:43 -0500

    Grouped Player Class
    Added Drawable and Updatable group and added player container variable.
    Also cleaned up main.py code for more readability.

--- main.py
+++ main.py
@@ -9,21 +9,32 @@ def main():
     pygame.init()
     Clock = pygame.time.Clock()
     dt = 0
+
+    Updatable = pygame.sprite.Group()
+    Drawable = pygame.sprite.Group()
+    Player.containers = (Updatable, Drawable)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) 
+
     print("Starting Asteroids!")
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         pygame.Surface.fill(screen, (0, 0, 0))
-        player.update(dt)
-        player.draw(screen)
+
+        for obj in Drawable:
+            obj.draw(screen)
+
+        Updatable.update(dt)
+
         pygame.display.flip()
+
         Clock.tick(60)
         dt = Clock.tick(60) / 1000
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
--- player.py
+++ player.py
@@ -7,7 +7,7 @@ class Player(CircleShape):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
 
-    # in the player class
+    # in the player clasS
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
